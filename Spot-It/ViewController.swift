0e67599637cd1c312240e578255ab80f0f6c8016
VyommBansal <vyommbansal@gmail.com>
2022-06-29 01:18:10 +0530

    sample run

--- Spot-It/ViewController.swift
+++ Spot-It/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+
 
 }
 
+class food {
+    var apple = "yum"
+}
