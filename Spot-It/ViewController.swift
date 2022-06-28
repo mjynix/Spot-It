e8d0d6ee3ac2a0465731a5e6ea94462fb3f2898b
Jasmine Ma <jasminema07@gmail.com>
2022-06-28 15:17:50 -0400

    Initial Commit

---
+++ Spot-It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spot-It
+//
+//  Created by Jasmine Ma on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
