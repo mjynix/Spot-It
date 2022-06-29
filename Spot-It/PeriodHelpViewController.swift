fdbdbae2439359153b73dcee3f002c60c9ae9b36
Jasmine Ma <jasminema07@gmail.com>
2022-06-29 14:46:24 -0400

    added period cycle info text

--- Spot-It/PeriodHelpViewController.swift
+++ Spot-It/PeriodHelpViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PeriodHelpViewController: UIViewController {
 
+    @IBOutlet weak var cycleInfo: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
